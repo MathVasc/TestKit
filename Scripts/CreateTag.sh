b6fe8e62b33179575c4ed701e29be803bdaae117
MathVasc <ma.vasconcelos98@gmail.com>
2020-09-26 23:43:53 -0300

    Fix workflow

--- Scripts/CreateTag.sh
+++ Scripts/CreateTag.sh
@@ -2,7 +2,10 @@
 
 set -o pipefail
 
-PODSPEC_FILE="MVTestKit.podspec"
+git config user.name github-actions
+git config user.email github-actions@github.com
+git pull origin master
+
 TAG=$(git for-each-ref refs/tags --sort=-taggerdate --format='%(refname:short)' --count=1)
 if ! [ -n "$TAG" ]; then
   TAG='0.0.0'
@@ -10,4 +13,6 @@ fi
 FIRST_TAG_COMPONENT=`echo $TAG | awk -F "." '{print $3}'`
 NEW_TAG=`echo $TAG | awk -F "." '{print '$(($FIRST_TAG_COMPONENT + 1))' "." $2 "." $3 }'`
 
-sed -i '' "s/$TAG/$NEW_TAG/" $PODSPEC_FILE
+git tag -a $NEW_TAG -m "New Release"
+git push origin $NEW_TAG
+
